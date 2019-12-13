928f3b587c63bbf84ef6e70297314bb02979eead
boniface193 <bonifacedennis99@gmail.com>
2019-12-13 13:11:20 -0800

    First Commit on Web clock

---
+++ asset/js/main.js
@@ -0,0 +1,13 @@
+const deg = 6;
+const hrs = document.querySelector('#hrs');
+const min = document.querySelector('#min');
+const sec = document.querySelector('#sec');
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+    hrs.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    min.style.transform = `rotateZ(${mm}deg)`;
+    sec.style.transform = `rotateZ(${ss}deg)`;
+})
